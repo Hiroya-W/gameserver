3ccd890e20677137ff86a5472c2c7c646f0e67d4
Hiroya-W <hyuyukun@gmail.com>
2022-09-11 13:41:36 +0000

    :+1: Register exceptions, handlers

--- app/api.py
+++ app/api.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
-from .exceptions import InvalidToken, InvalidTokenHandler
+from .exceptions import (
+    InvalidJudgeResult,
+    InvalidJudgeResultHandler,
+    InvalidToken,
+    InvalidTokenHandler,
+    RoomNotFound,
+    RoomNotFoundHandler,
+)
 from .routers import room, user
 
 app = FastAPI()
@@ -8,6 +15,8 @@ app.include_router(user.router)
 app.include_router(room.router)
 
 app.add_exception_handler(InvalidToken, InvalidTokenHandler)
+app.add_exception_handler(RoomNotFound, RoomNotFoundHandler)
+app.add_exception_handler(InvalidJudgeResult, InvalidJudgeResultHandler)
 
 
 # Sample APIs
